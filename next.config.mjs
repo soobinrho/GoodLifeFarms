1d72015f180f3cafda7e7127d1617b6cbad86585
Soobin Rho <soobinrho@gmail.com>
2024-02-17 15:49:10 -0600

    refactor: allow inline scripts from CSP header
    Signed-off-by: Soobin Rho <soobinrho@gmail.com>
    Co-Authored-By: Kathy Do <kathykhado@gmail.com>
    Co-Authored-By: Tobi Onimole <tobionimole50@gmail.com>

--- next.config.mjs
+++ next.config.mjs
@@ -12,7 +12,7 @@ const nextConfig = {
           {
             key: 'Content-Security-Policy',
             value:
-              "default-src 'none'; script-src 'self'; connect-src 'self'; img-src 'self'; style-src 'self'; font-src 'self' https://fonts.googleapis.com; worker-src 'self'; base-uri 'self'; form-action 'self'; manifest-src 'self'; object-src 'self';",
+              "default-src 'none'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; connect-src 'self'; img-src 'self'; style-src 'self' 'unsafe-inline'; font-src 'self' https://fonts.googleapis.com; worker-src 'self'; base-uri 'self'; form-action 'self'; manifest-src 'self'; object-src 'self';",
           },
           {
             key: 'X-Content-Type-Options',
