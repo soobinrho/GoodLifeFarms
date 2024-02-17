2abe59fafa0a0bb73f6b94e14f1452f423be7254
Soobin Rho <soobinrho@gmail.com>
2024-02-17 10:38:06 -0600

    feat: add CSP to prevent XSS
    Source:
      https://csp.withgoogle.com/docs/strict-csp.html
    
    Signed-off-by: Soobin Rho <soobinrho@gmail.com>
    Co-Authored-By: Kathy Do <kathykhado@gmail.com>
    Co-Authored-By: Tobi Onimole <tobionimole50@gmail.com>

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,37 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'X-Frame-Options',
+            value: 'DENY',
+          },
+          {
+            key: 'Content-Security-Policy',
+            value:
+              "default-src 'none'; script-src 'self'; connect-src 'self'; img-src 'self'; style-src 'self'; font-src 'self' https://fonts.googleapis.com; worker-src 'self'; base-uri 'self'; form-action 'self'; manifest-src 'self'; object-src 'self';",
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            key: 'Permissions-Policy',
+            // Prevent XSS attacks. The following line disallows access
+            // to camera, geolocation, and microphone.
+            value: 'camera=(), geolocation=(), microphone=()',
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'origin-when-cross-origin',
+          },
+        ],
+      },
+    ];
+  },
+};
 
 export default nextConfig;
