5aa6a8fb4b72ee846e1c619658f8696e8eea162d
Soobin Rho <soobinrho@gmail.com>
2024-02-18 00:27:18 -0600

    refactor: install shadcn-ui for table component (MIT License)
    Signed-off-by: Soobin Rho <soobinrho@gmail.com>

--- src/components/prototype-page.tsx
+++ src/components/prototype-page.tsx
@@ -1,8 +1,36 @@
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/table';
+
 export default function PrototypePage() {
   return (
     <>
       <div className='relative h-max min-h-screen overflow-y-auto bg-background'>
-        <section className='m-16'>hello world</section>
+        <Table className='mx-10 my-14'>
+          <TableCaption>A list of your recent invoices.</TableCaption>
+          <TableHeader>
+            <TableRow>
+              <TableHead className='w-[100px]'>Invoice</TableHead>
+              <TableHead>Status</TableHead>
+              <TableHead>Method</TableHead>
+              <TableHead className='text-right'>Amount</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            <TableRow>
+              <TableCell className='font-medium'>INV001</TableCell>
+              <TableCell>Paid</TableCell>
+              <TableCell>Credit Card</TableCell>
+              <TableCell className='text-right'>$250.00</TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
       </div>
     </>
   );
