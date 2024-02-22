bc7b75aff7688319376fcddd105c1551182ef2b2
Soobin Rho <soobinrho@gmail.com>
2024-02-21 22:00:10 -0600

    feat: open in Google Sheets button
    Signed-off-by: Soobin Rho <soobinrho@gmail.com>

--- src/app/prototype/page.tsx
+++ src/app/prototype/page.tsx
@@ -1,5 +1,6 @@
 import Footer from '@/components/footer';
 import HeaderGoBackToHome from '@/components/header-go-back-to-home';
+import HeaderGoogleSheets from '@/components/header-google-sheets';
 import HeaderSocialMedia from '@/components/header-social-media';
 import PrototypePage from '@/components/prototype-page';
 
@@ -7,6 +8,7 @@ export default function Page() {
   return (
     <main className='relative min-h-screen w-full text-primary'>
       <HeaderGoBackToHome />
+      <HeaderGoogleSheets/>
       <HeaderSocialMedia />
       <PrototypePage />
       <Footer />
---
+++ src/components/header-google-sheets.tsx
@@ -0,0 +1,32 @@
+'use client';
+
+export default function HeaderGoogleSheets() {
+  return (
+    <div className='absolute left-[50%] z-30 mt-4 flex translate-x-[-50%] gap-2 text-center'>
+      <a
+        target='_blank'
+        href='https://docs.google.com/spreadsheets/d/11_5BPnlA3c1OWxi3f5ej0adNLs6DXseL7qeKPvOlJyU/edit?usp=sharing'
+        rel='noopener noreferrer'
+      >
+        <svg
+          className='fill-current hover:text-primary-3/70 active:text-black/70'
+          xmlns='http://www.w3.org/2000/svg'
+          width='24'
+          height='24'
+          viewBox='0 0 24 24'
+        >
+          <path d='M11.318 12.545H7.91v-1.909h3.41v1.91zM14.728 0v6h6l-6-6zm1.363 10.636h-3.41v1.91h3.41v-1.91zm0 3.273h-3.41v1.91h3.41v-1.91zM20.727 6.5v15.864c0 .904-.732 1.636-1.636 1.636H4.909a1.636 1.636 0 0 1-1.636-1.636V1.636C3.273.732 4.005 0 4.909 0h9.318v6.5h6.5zm-3.273 2.773H6.545v7.909h10.91v-7.91zm-6.136 4.636H7.91v1.91h3.41v-1.91z' />
+        </svg>
+      </a>
+      <a
+        target='_blank'
+        href='https://docs.google.com/spreadsheets/d/11_5BPnlA3c1OWxi3f5ej0adNLs6DXseL7qeKPvOlJyU/edit?usp=sharing'
+        rel='noopener noreferrer'
+      >
+        <span className='font-extralight hover:text-primary-3/70 active:text-black/70'>
+          Open in Google Sheets
+        </span>
+      </a>
+    </div>
+  );
+}
