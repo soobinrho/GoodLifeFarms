e2135e8999a8bc8c054c3339469800d178cb10c6
Soobin Rho <soobinrho@gmail.com>
2024-10-06 12:33:18 -0500

    refactor: hash value for style CSP
    Source:
      https://nextjs.org/docs/app/building-your-application/configuring/content-security-policy
    Signed-off-by: Soobin Rho <soobinrho@gmail.com>

--- next.config.mjs
+++ next.config.mjs
@@ -11,8 +11,20 @@ const nextConfig = {
           },
           {
             key: 'Content-Security-Policy',
+            // Whenever style and script contents are chagned, new hashes need to be added.
+            // Use Chrome Developer's Tool - Console to find the hash values.
             value:
-              "default-src 'none'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; connect-src 'self'; img-src 'self'; style-src 'self' 'unsafe-inline'; font-src 'self' https://fonts.googleapis.com; worker-src 'self'; base-uri 'self'; form-action 'self'; manifest-src 'self'; object-src 'self';",
+              "img-src 'self' data: blob:; \
+              style-src 'self' 'sha256-RpGvlRbRQPlLZDBLDKCjN1VY9+ac/RHqgjmDHc2Y6PA=' 'sha256-47DEQpj8HBSa+/TImW+5JCeuQeRkm5NMpJWZG3hSuFU='; \
+              font-src 'self' https://fonts.googleapis.com; \
+              worker-src 'self' blob:; \
+              base-uri 'self'; \
+              form-action 'self'; \
+              manifest-src 'self'; \
+              object-src 'none'; \
+              child-src blob:; \
+              connect-src 'self'; \
+              script-src 'self' 'unsafe-inline' 'unsafe-eval';",
           },
           {
             key: 'X-Content-Type-Options',
