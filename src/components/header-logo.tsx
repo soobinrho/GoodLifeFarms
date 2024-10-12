ff9dce18763e82ef8343b362e348c2e7c9ef982e
Soobin Rho <soobinrho@gmail.com>
2024-10-12 16:54:30 -0500

    refactor: variable size for logo depending on screen size
    Signed-off-by: Soobin Rho <soobinrho@gmail.com>

--- src/components/header-logo.tsx
+++ src/components/header-logo.tsx
@@ -7,21 +7,12 @@ export default function HeaderLogo() {
   return (
     <>
       <Link
-        className='invisible absolute start-0 z-30 ml-5 mt-3 w-36 overflow-visible sm:visible'
+        className='absolute start-0 z-30 ml-4 mt-3 w-20 overflow-visible sm:ml-5 sm:mt-3 sm:w-36'
         href='/'
       >
         <img
           alt='Logo of Good Life Farms. Orange Puppy.'
           src='/2024-10-11_logo.svg'
-        />
-      </Link>
-      <Link
-        className='visible absolute start-0 z-30 ml-4 mt-2 w-14 overflow-visible sm:invisible'
-        href='/'
-      >
-        <img
-          alt='Logo of Good Life Farms. Orange Puppy.'
-          src='/2024-10-11_orange_puppy.svg'
         />
       </Link>
     </>
