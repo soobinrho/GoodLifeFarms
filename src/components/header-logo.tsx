496e0f4739d2ebcde83be9069a1f74056e2b3b0e
Soobin Rho <soobinrho@gmail.com>
2024-10-12 17:16:56 -0500

    fix: logo vertical not aligned in mobile view
    Signed-off-by: Soobin Rho <soobinrho@gmail.com>

--- src/components/header-logo.tsx
+++ src/components/header-logo.tsx
@@ -7,7 +7,7 @@ export default function HeaderLogo() {
   return (
     <>
       <Link
-        className='absolute start-0 z-30 ml-4 mt-3 w-20 overflow-visible sm:ml-5 sm:mt-3 sm:w-36'
+        className='absolute start-0 z-30 ml-4 mt-3 w-20 overflow-visible pt-0.5 sm:ml-5 sm:mt-3 sm:w-36 sm:pt-0'
         href='/'
       >
         <img
